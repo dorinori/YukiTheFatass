c9e1734ce96df8955ef6d12a0187a0ff97fc6abb
Doris Wei <34874473+dorinori@users.noreply.github.com>
2025-04-14 14:02:37 -0700

    Initial Commit

---
+++ YukiTheFatass/YukiTheFatassApp.swift
@@ -0,0 +1,17 @@
+//
+//  YukiTheFatassApp.swift
+//  YukiTheFatass
+//
+//  Created by Doris Wei on 4/14/25.
+//
+
+import SwiftUI
+
+@main
+struct YukiTheFatassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ YukiTheFatassTests/YukiTheFatassTests.swift
@@ -0,0 +1,17 @@
+//
+//  YukiTheFatassTests.swift
+//  YukiTheFatassTests
+//
+//  Created by Doris Wei on 4/14/25.
+//
+
+import Testing
+@testable import YukiTheFatass
+
+struct YukiTheFatassTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
